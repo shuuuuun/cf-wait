1a53c6c22bbc4a0163ad234e778fec2b8bbd3b56
motoki-shun <shuuuuuny+github@gmail.com>
2019-10-14 20:50:51 +0900

    impliment select_distribution

--- lib/cf_wait/cli.ex
+++ lib/cf_wait/cli.ex
@@ -10,7 +10,8 @@ defmodule CfWait.CLI do
     case parse do
       { [ help: true ], _, _ } -> :help
       # { _, [ user, project, count ], _ } -> { user, project, String.to_integer(count) }
-      _ -> :list_distributions
+      # _ -> :list_distributions
+      _ -> :run
       # _ -> :help
     end
   end
@@ -22,13 +23,38 @@ defmodule CfWait.CLI do
     System.halt(0)
   end
   def process(:run) do
-    # list_distributions
-    # |> select_distribution
+    list_distributions()
+    |> select_distribution
+    |> IO.inspect
     # |> wait_deployed
     # |> notify_deployed
   end
   def process(:list_distributions) do
-    CfWait.CloudFront.list_distributions
-    |> ExAws.request!
+    list_distributions()
+  end
+
+  defp list_distributions do
+    res =
+      CfWait.CloudFront.list_distributions
+      |> ExAws.request!
+    # IO.puts "res: #{inspect res}"
+    res.body.items
+  end
+
+  defp select_distribution(distributions) do
+    distributions
+    |> Enum.with_index
+    |> Enum.each(fn {dist, index} ->
+      IO.puts "#{index}: #{inspect dist}"
+    end)
+    index_list = 0..(Enum.count(distributions)-1) |> Enum.join("/")
+    # selected = IO.gets("which distribution? (#{index_list}) ") |> String.trim |> String.to_integer
+    selected = IO.gets("which distribution? (#{index_list}) ") |> String.trim
+    selected_num =
+      case Integer.parse(selected) do
+        {num, _} -> num
+        :error -> 0
+      end
+    Enum.at distributions, selected_num
   end
 end
